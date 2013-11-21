ac434ebe3ac0f0d0d9eba534ecd5f5c2809028cb
Jordan Craig <jordan.craig94@gmail.com>
2013-11-21 19:00:44 +0000

    Reqrite of Jokr using EJS for view injection and bootsrap CSS

---
+++ app.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+var host = "127.0.0.1";
+var port = 1337;
+var express = require("express");
+
+var app = express();
+app.use(app.router); //use both root and other routes below
+app.use(express.static(__dirname)); //use static files in ROOT/public folder
+app.set('views', __dirname);
+app.engine('html', require('ejs').renderFile);
+
+app.get("/", function(req, res){ //root dir
+    res.render('index.html');
+});
+
+app.listen(port, host);
--- jokr/jokr.io.js
+++ jokr/jokr.io.js
@@ -1,61 +1,21 @@
-function UserSignIn(a, t) {
+// Create user sign-in view
+function UserSignIn(t, a) {
 	this.a = a;
 	this.t = t;
 
 	this.create = function() {
 
-		var f = document.createElement("form")
+		// Create EJS reference
+		template = new EJS({url: 'jokr/templates/user-sign-in.ejs'}).render();
 
-		f.setAttribute('method', 'post');
-		f.setAttribute('action', this.a);
+		// Render template
+		$('body').html(template);
 
-		
-		var h = document.createElement("h1");
-		h.innerHTML = this.t;
-		var hdiv = document.createElement("div");
-		hdiv.appendChild(h);
 
-		var i = document.createElement("input");
-		i.type = "text";
-		i.setAttribute('placeholder', 'Username');
-		i.id = "username";
-		var idiv = document.createElement("div");
-		idiv.appendChild(i);
-
-		var p = document.createElement("input");
-		p.type = "password";
-		p.setAttribute('placeholder', 'Password');
-		p.id = "password";
-		var pdiv = document.createElement("div");
-		pdiv.appendChild(p);
-
-		var s = document.createElement("input");
-		s.type = "button";
-		s.id = 'login';
-		s.value = "Log in";
-
-		var l = document.createElement("a");
-		l.setAttribute('href', '#');
-		l.innerHTML = "Lost your password?"
-
-		var r = document.createElement("a");
-		r.setAttribute('href', '#');
-		r.innerHTML = "Register"
-
-		var sdiv = document.createElement("div");
-		sdiv.appendChild(s);
-		sdiv.appendChild(l);
-		sdiv.appendChild(r);
-
-		f.appendChild(hdiv);
-		f.appendChild(idiv);
-		f.appendChild(pdiv);
-		f.appendChild(sdiv);
-
-		$('#content').append(f);
-
+		// Listen for click events on login button
 		$('#login').on("click", FireEvent);
 
+		// When login button is clicked, fire off event to alert front-end logic.
 		function FireEvent() {
 			$.event.trigger({
 				type: "loginSubmitted",
@@ -63,16 +23,20 @@ function UserSignIn(a, t) {
 		}
 	}
 
+	// Form Helpers
+	// Check if password field is empty
 	this.isPasswordEmpty = function () {
 		if ($('#password').val().length == 0 ) { return true; } 
 	}
 
+	//Check if password length is less than 'X' value
 	this.isLessThanX = function(x) {
 		if ($('#password').val().length < x ) { return true; }
 	}
 
 }
 
+// Create custom alert
 function Alert(opts) {
 	alert(opts);
 }
